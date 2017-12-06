949290d040c37cac80fd292fe181961bd9ff396c
Michael <mgh0010@auburn.edu>
2017-12-05 23:23:54 -0600

    latest

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const director = require('director');
 const Server   = require('./lib/server');
 
 // Create a router for GET and POST requests to the app
-const router = director.http.Router({
+const router = new director.http.Router({
     '/': {
         post: Server.postResponse,
         get: Server.getResponse
